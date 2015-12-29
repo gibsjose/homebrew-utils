c977baa4e5eb387a3829abc1e143015f32e9117f
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-12-29 15:17:51 -0500

    Updating bcrypt-hash formula

--- bcrypt-hash.rb
+++ bcrypt-hash.rb
@@ -1,9 +1,13 @@
 class BcryptHhash < Formula
-  homepage "https://github.com/gibsjose/bcrypt-hash"
-  url "https://github.com/gibsjose/bcrypt-hash/archive/1.1.0.tar.gz"
-  sha256 "d0b7bedc93ed31398f4f5e1c68b447971fd865dcb948c97a1042254c003d8766"
+    desc "Generate or verify secure cryptographic hashes from the command-line using bcrypt"
+    homepage "https://github.com/gibsjose/bcrypt-hash"
+    version "1.1.0"
+    url "https://github.com/gibsjose/bcrypt-hash/archive/1.1.0.tar.gz"
+    sha256 "d0b7bedc93ed31398f4f5e1c68b447971fd865dcb948c97a1042254c003d8766"
 
-  def install
-    bin.install "bcrypt-hash-1.1.0" => "bcrypt-hash"
-  end
+    bottle :unneeded
+
+    def install
+        bin.install "bcrypt-hash-1.1.0" => "bcrypt-hash"
+    end
 end
