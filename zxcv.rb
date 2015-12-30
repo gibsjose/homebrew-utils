15a65a4949df209d7a986dc56a60600b1ead7d5f
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-12-29 23:39:02 -0500

    Updating zxcv to use correct sha-256 checksum

--- zxcv.rb
+++ zxcv.rb
@@ -3,7 +3,7 @@ class Zxcv < Formula
   homepage "https://github.com/gibsjose/zxcv"
   url "https://github.com/gibsjose/zxcv/archive/1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "34d9ea3081f950b37458a61d2744bacf55fce3af6c1cdca5f43709590434b439"
+  sha256 "2be328433e559bc25c207348d1ca9a08d422e4a63c1d484c81c72fd1698ce551"
 
   def install
       bin.install "zxcv"
