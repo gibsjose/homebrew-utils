a15279a1b12044dd1209fb3b074e389097eafb17
Joe Gibson <gibsjose@mail.gvsu.edu>
2016-01-23 20:08:37 -0500

    Updating nacronym to 1.4.2

--- nacronym.rb
+++ nacronym.rb
@@ -1,9 +1,9 @@
 class Nacronym < Formula
   desc "NASA Acronym Search Tool"
   homepage "https://github.com/gibsjose/nacronym"
-  url "https://github.com/gibsjose/nacronym/archive/1.4.1.tar.gz"
-  version "1.4.0"
-  sha256 "bed5d1d9a55794b8ff9a91e23fda75451111ae3fc0bfa80a993e9126445a2269"
+  url "https://github.com/gibsjose/nacronym/archive/1.4.2.tar.gz"
+  version "1.4.2"
+  sha256 "db10820977d691409f0f72f08e7726176ba1185040277b5413b04e3d5ee136c5"
 
   def install
       bin.install "nacronym"
